c875350e77edf87fa49301c7cdde51f4a4b70a6d
Savy92 Saddall <savy92@hotmail.co.uk>
2024-03-08 03:49:13 +0000

    create custom hook for CurrentUserContext

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,8 +15,11 @@ import axios from "axios";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
+    const setCurrentUser = useSetCurrentUser();
+
     const [signInData, setSignInData] = useState({
         username: "",
         password: "",
@@ -29,7 +32,8 @@ function SignInForm() {
       const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-          await axios.post("/dj-rest-auth/login/", signInData);
+          const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+          setCurrentUser(data.user);
           history.push("/");
         } catch (err) {
             setErrors(err.response?.data)
